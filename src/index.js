38e49712a37270e6285ccc64c4740aa66c4e34f4
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-06-09 14:52:33 -0400

    my first commit

--- src/index.js
+++ src/index.js
@@ -16,14 +16,33 @@ const books = [
   },
 ];
 
-
-
 const BookList = () => {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book, index) => (
         <Book key={index} {...book} />
       ))}
+    </section>
+  );
+};
+
+const EventExamples = () => {
+  const handleForm = () => {
+    console.log("handel form input");
+  };
+
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input type="text" name="example" onChange={handleForm} style={{ margin: "1rem 0" }}></input>
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
     </section>
   );
 };
