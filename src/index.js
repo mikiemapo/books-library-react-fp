210e421b63469e842eaef654f9711732fa38bda4
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-06-09 14:24:11 -0400

    my first set up

--- src/index.js
+++ src/index.js
@@ -2,38 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const firstBook ={
-Author : 'Gregory E. Lang, Susanna Leonard Hill',
-Title : 'Why a Daughter Needs a Dad',
-Img : './images/book-1.jpg'
-}
+const books = [
+  {
+    Author: "Gregory E. Lang, Susanna Leonard Hill",
+    Title: "Why a Daughter Needs a Dad",
+    Img: "./images/book-1.jpg",
+  },
 
-const secondBook ={
-  Author : 'by Kahran Bethencourt, Regis Bethencourt',
-  Title : 'CROWNED: Magical Folk and Fairy Tales from the Diaspora',
-  Img : './images/book-2.jpg'
-  }
-  
+  {
+    Author: "by Kahran Bethencourt, Regis Bethencourt",
+    Title: "CROWNED: Magical Folk and Fairy Tales from the Diaspora",
+    Img: "./images/book-2.jpg",
+  },
+];
+
 
 
 const BookList = () => {
   return (
     <section className="booklist">
-    <Book Author={firstBook.Author} Img ={firstBook.Img} Title={firstBook.Title}/>
-    <Book Author={secondBook.Author} Img ={secondBook.Img} Title={secondBook.Title}/>
-    <Book Author={firstBook.Author} Img ={firstBook.Img} Title={firstBook.Title}/>
-    <Book Author={secondBook.Author} Img ={secondBook.Img} Title={secondBook.Title}/>
+      {books.map((book, index) => (
+        <Book key={index} {...book} />
+      ))}
     </section>
   );
 };
 
 const Book = (props) => {
-  console.log(props)
+  const { Img, Title, Author } = props;
   return (
     <article className="book">
-      <img src={props.Img} alt={props.Title} />
-      <h2>{props.Title}</h2>
-      <h4>{props.Author}</h4>
+      <img src={Img} alt={Title} />
+      <h2>{Title}</h2>
+      <h4>{Author}</h4>
+
       {console.log(props)}
     </article>
   );
